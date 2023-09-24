86adf5d0f73e69e37039cdecb9f8aed8dc4846d8
mazerman03 <105751347+mazerman03@users.noreply.github.com>
2023-09-24 02:51:46 -0600

    Chat history (No web implementation tho)

--- demopdf.py
+++ demopdf.py
@@ -77,3 +77,26 @@ import ipywidgets as widgets
 
 # Create conversation chain that uses our vectordb as retriver, this also allows for chat history management
 qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.1), db.as_retriever())
+
+chat_history = []
+
+def on_submit(_):
+    query = input_box.value
+    input_box.value = ""
+    
+    if query.lower() == 'exit':
+        print("Thank you for using the State of the Union chatbot!")
+        return
+    
+    result = qa({"question": query, "chat_history": chat_history})
+    chat_history.append((query, result['answer']))
+    
+    display(widgets.HTML(f'<b>User:</b> {query}'))
+    display(widgets.HTML(f'<b><font color="blue">Chatbot:</font></b> {result["answer"]}'))
+
+print("Welcome to the Transformers chatbot! Type 'exit' to stop.")
+
+input_box = widgets.Text(placeholder='Please enter your question:')
+input_box.on_submit(on_submit)
+
+display(input_box)
